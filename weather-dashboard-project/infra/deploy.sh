d5298fce6d7d0b624717c2ec8d8f56d3ef690c12
chriantho <chrishelle.anthoneyz@gmail.com>
2025-10-04 16:03:46 +1000

    Delete weather-dashboard-project directory

--- weather-dashboard-project/infra/deploy.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-set -e
-APP_PATH=/home/ubuntu/weather-dashboard
-
-ssh $DEPLOY_USER@$DEPLOY_HOST "mkdir -p $APP_PATH"
-scp -r * $DEPLOY_USER@$DEPLOY_HOST:$APP_PATH/
-
-ssh $DEPLOY_USER@$DEPLOY_HOST "cd $APP_PATH && docker compose up -d --build"
